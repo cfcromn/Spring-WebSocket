bfea989a0fb730460019e958d2b79a88a5d2dcf3
박하민 <hamin090403@gmail.com>
2025-09-04 13:01:26 +0900

    Spring-WebSocket

--- src/main/java/com/example/chat/config/WebSocketConfig.java
+++ src/main/java/com/example/chat/config/WebSocketConfig.java
@@ -1,7 +1,27 @@
 package com.example.chat.config;
 
 
+import com.example.chat.config.handler.ChatWebSocketHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
-public class WebSocketConfig {
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final ChatWebSocketHandler chatWebSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        System.out.println("[+] 최초 WebSocket 연결을 위한 등록 Handler");
+        registry
+                // 클라이언트에서 웹 소켓 연결을 위해 "ws-stomp"라는 엔드포인트에 연결을 시도하면 ChatWebSocketHandler 클래스에서 처리함
+                .addHandler(chatWebSocketHandler, "ws-stomp")
+                .setAllowedOrigins("*");
+    }
 
 }
--- src/main/java/com/example/chat/config/handler/ChatWebSocketHandler.java
+++ src/main/java/com/example/chat/config/handler/ChatWebSocketHandler.java
@@ -1,4 +1,8 @@
 package com.example.chat.config.handler;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ChatWebSocketHandler {
+
 }
